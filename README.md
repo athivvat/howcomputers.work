3bf15c36609b36f43f895f6bb4467297b3f2e43d
athivvat <athivvat@gmail.com>
2024-10-31 23:39:34 +0700

    Initial

---
+++ README.md
@@ -0,0 +1,3 @@
+# How Computers Work?
+
+The best way to understand how computers work is to build one from scratch.
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	let displayText = 'How Computers Work?';
+	let cursorVisible = true;
+
+	setInterval(() => {
+		cursorVisible = !cursorVisible;
+	}, 500);
+</script>
+
+<div class="flex min-h-screen items-center justify-center bg-black text-green-500">
+	<div class="flex items-center font-mono">
+		<span>{displayText}</span>
+		<div class="w-10 -mt-0.5">
+            <span class={cursorVisible ? 'inline-block' : 'hidden'}>|</span>
+		</div>
+	</div>
+</div>
